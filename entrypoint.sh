14a39415b3e0f63288e1243c8afab9ad1b0ad7d2
iamangus <turnbull.angus@gmail.com>
2022-02-26 17:37:22 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,10 +24,10 @@ else
   echo "Found github repo. Checking for a github personal access token."
   if [[ -z "${GH_TOKEN}" ]]; then
     echo "No github token provided. Cloning public repo."
-    git clone https://$${GH_REPO}
+    git clone $GH_REPO
   else
     echo "found a github token. Cloning repo."
-    git clone https://${GH_TOKEN}@${GH_REPO}
+    git clone $GH_TOKEN@$GH_REPO
   fi
 fi
 
